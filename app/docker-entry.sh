b4028c06ff5ea8756a19213b09b18abfcc861ba8
Enrico <rico.bl@gmail.com>
2016-10-06 14:43:48 -0300

    Removed database wait

--- app/docker-entry.sh
+++ app/docker-entry.sh
@@ -6,15 +6,6 @@ cp /config.template.json $config
 sed -Ei "s/DBAAS_MYSQL_ENDPOINT/$DBAAS_MYSQL_ENDPOINT/" $config
 echo OK
 
-echo "Wait until database $DB_HOST:$DB_PORT_5432_TCP_PORT is ready..."
-until nc -z $DB_HOST $DB_PORT_5432_TCP_PORT
-do
-    sleep 1
-done
-
-# Wait to avoid "panic: Failed to open sql connection pq: the database system is starting up"
-sleep 1
-
 echo "Starting platform"
 cd /mattermost/bin
 ./platform $*
