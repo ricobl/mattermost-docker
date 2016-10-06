e76c66149baa5f259e320c13959959bd1ffa4353
Enrico <rico.bl@gmail.com>
2016-10-06 15:02:32 -0300

    Fixed mysql endpoint copy from env to config

--- app/docker-entry.sh
+++ app/docker-entry.sh
@@ -3,7 +3,7 @@ config=/mattermost/config/config.json
 
 echo -ne "Configure database connection..."
 cp /config.template.json $config
-sed -Ei "s/DBAAS_MYSQL_ENDPOINT/$DBAAS_MYSQL_ENDPOINT/" $config
+sed -i -e "s#DBAAS_MYSQL_ENDPOINT#$DBAAS_MYSQL_ENDPOINT#" $config
 echo OK
 
 echo "Starting platform"
