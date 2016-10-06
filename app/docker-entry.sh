7a5b99d89f05870a1d7c5ddd70d9afe022f4cdb6
Enrico <rico.bl@gmail.com>
2016-10-06 15:54:37 -0300

    Removed "mysql://" protocol from the endpoint

--- app/docker-entry.sh
+++ app/docker-entry.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+
 config=/mattermost/config/config.json
+NO_PROTOCOL_DBAAS_MYSQL_ENDPOINT=`echo $DBAAS_MYSQL_ENDPOINT | sed -e 's#mysql://##'`
 
 echo -ne "Configure database connection..."
 cp /config.template.json $config
-sed -i -e "s#DBAAS_MYSQL_ENDPOINT#$DBAAS_MYSQL_ENDPOINT#" $config
+sed -i -e "s#DBAAS_MYSQL_ENDPOINT#$NO_PROTOCOL_DBAAS_MYSQL_ENDPOINT#" $config
 echo OK
 
 echo "Starting platform"
