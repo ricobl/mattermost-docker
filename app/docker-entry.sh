609a73c0130e2fed6e2a5fb36abfba0df1a3e662
Enrico <rico.bl@gmail.com>
2016-10-06 16:41:15 -0300

    Wrapped host of mysql endpoint in `tcp()`

--- app/docker-entry.sh
+++ app/docker-entry.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
 config=/mattermost/config/config.json
-NO_PROTOCOL_DBAAS_MYSQL_ENDPOINT=`echo $DBAAS_MYSQL_ENDPOINT | sed -e 's#mysql://##'`
+
+_parse_endpoint() {
+    endpoint=$1
+    # Remove protocol
+    endpoint=$(echo $endpoint | sed -e 's#mysql://##')
+    # Wrap domain in @tcp()
+    endpoint=$(echo $endpoint | sed -e 's#@\([^/]*\)/#@tcp(\1)/#')
+    echo $endpoint
+}
+
+PARSED_DBAAS_MYSQL_ENDPOINT=`_parse_endpoint $DBAAS_MYSQL_ENDPOINT`
 
 echo -ne "Configure database connection..."
 cp /config.template.json $config
-sed -i -e "s#DBAAS_MYSQL_ENDPOINT#$NO_PROTOCOL_DBAAS_MYSQL_ENDPOINT#" $config
+sed -i -e "s#DBAAS_MYSQL_ENDPOINT#$PARSED_DBAAS_MYSQL_ENDPOINT#" $config
 echo OK
 
 echo "Starting platform"
