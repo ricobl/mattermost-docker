adbae6e218645605f2dca2156497b580fceae5c9
Enrico <rico.bl@gmail.com>
2016-10-06 13:52:46 -0300

    Changed database env variables and forced config regeneration

--- app/docker-entry.sh
+++ app/docker-entry.sh
@@ -1,23 +1,10 @@
 #!/bin/bash
 config=/mattermost/config/config.json
-DB_HOST=${DB_HOST:-db}
-DB_PORT_5432_TCP_PORT=${DB_PORT_5432_TCP_PORT:-5432}
-MM_USERNAME=${MM_USERNAME:-mmuser}
-MM_PASSWORD=${MM_PASSWORD:-mmuser_password}
-MM_DBNAME=${MM_DBNAME:-mattermost}
+
 echo -ne "Configure database connection..."
-if [ ! -f $config ]
-then
-    cp /config.template.json $config
-    sed -Ei "s/DB_HOST/$DB_HOST/" $config
-    sed -Ei "s/DB_PORT/$DB_PORT_5432_TCP_PORT/" $config
-    sed -Ei "s/MM_USERNAME/$MM_USERNAME/" $config
-    sed -Ei "s/MM_PASSWORD/$MM_PASSWORD/" $config
-    sed -Ei "s/MM_DBNAME/$MM_DBNAME/" $config
-    echo OK
-else
-    echo SKIP
-fi
+cp /config.template.json $config
+sed -Ei "s/DBAAS_MYSQL_ENDPOINT/$DBAAS_MYSQL_ENDPOINT/" $config
+echo OK
 
 echo "Wait until database $DB_HOST:$DB_PORT_5432_TCP_PORT is ready..."
 until nc -z $DB_HOST $DB_PORT_5432_TCP_PORT
